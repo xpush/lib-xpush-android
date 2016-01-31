ce0ccb5bc8bf02c870ba6c2a5ac177a10057a650
0nlyoung7 <0nlyoung7@gmail.com>
2016-01-31 15:57:30 +0900

    Add zoomable event in imageviewer

--- sample/src/main/java/io/xpush/sampleChat/activities/ImageViewerActivity.java
+++ sample/src/main/java/io/xpush/sampleChat/activities/ImageViewerActivity.java
@@ -1,38 +1,29 @@
 package io.xpush.sampleChat.activities;
 
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.net.Uri;
-import android.os.AsyncTask;
+import android.graphics.Matrix;
+import android.graphics.PointF;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.util.FloatMath;
 import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
-import com.facebook.drawee.view.SimpleDraweeView;
+import com.bumptech.glide.load.resource.drawable.GlideDrawable;
+import com.bumptech.glide.request.animation.GlideAnimation;
+import com.bumptech.glide.request.target.GlideDrawableImageViewTarget;
 
-import java.io.InputStream;
 import java.util.ArrayList;
 
-import io.xpush.chat.models.XPushChannel;
 import io.xpush.sampleChat.R;
 
 public class ImageViewerActivity extends AppCompatActivity {
@@ -42,8 +33,7 @@ public class ImageViewerActivity extends AppCompatActivity {
     ImageFragmentPagerAdapter imageFragmentPagerAdapter;
     ViewPager viewPager;
     static ArrayList<String> mUriList = new ArrayList<String>();
-
-    private Toolbar mToolbar;
+    ImageView mBtnClose;
 
     ViewPager.OnPageChangeListener onPageChangeListener = new ViewPager.OnPageChangeListener() {
         public void onPageScrollStateChanged(int paramAnonymousInt) {
@@ -68,38 +58,26 @@ public class ImageViewerActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_image_viewer);
 
-        mToolbar = (Toolbar) findViewById(R.id.toolbar);
-        mToolbar.setTitle(getString(R.string.title_photo)) ;
-        setSupportActionBar(mToolbar);
-
         Intent intent = getIntent();
         String imageUri = intent.getStringExtra("imageUri");
-        mUriList.add( imageUri );
+        if( mUriList.indexOf( imageUri  ) < 0 ) {
+            mUriList.add(imageUri);
+        }
 
         imageFragmentPagerAdapter = new ImageFragmentPagerAdapter(getSupportFragmentManager());
         viewPager = (ViewPager) findViewById(R.id.viewPager);
         viewPager.setAdapter(imageFragmentPagerAdapter);
 
         this.viewPager.addOnPageChangeListener(this.onPageChangeListener);
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu_image_viewer, menu);
-        return true;
-    }
+        mBtnClose = (ImageView) findViewById(R.id.btnClose);
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-
-        if (id == R.id.action_close) {
-            this.finish();
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+        mBtnClose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 
     public static class ImageFragmentPagerAdapter extends FragmentPagerAdapter {
@@ -118,21 +96,51 @@ public class ImageViewerActivity extends AppCompatActivity {
         }
     }
 
-    public static class SwipeFragment extends Fragment {
+    public static class SwipeFragment extends Fragment implements View.OnTouchListener {
+
+        @SuppressWarnings("unused")
+        private static final float MIN_ZOOM = 1f,MAX_ZOOM = 1f;
+
+        // These matrices will be used to scale points of the image
+        Matrix matrix = new Matrix();
+        Matrix savedMatrix = new Matrix();
+        Matrix mMinScaleMatrix = new Matrix();
+
+        // The 3 states (events) which the user is trying to perform
+        static final int NONE = 0;
+        static final int DRAG = 1;
+        static final int ZOOM = 2;
+        int mode = NONE;
+
+        // these PointF objects are used to record the point(s) the user is touching
+        PointF start = new PointF();
+        PointF mid = new PointF();
+        float oldDist = 1f;
+
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
             View swipeView = inflater.inflate(R.layout.swipe_fragment, container, false);
-            ImageView imageView = (ImageView) swipeView.findViewById(R.id.imageView);
+            final ImageView imageView = (ImageView) swipeView.findViewById(R.id.imageView);
+
+            imageView.setOnTouchListener(this);
             Bundle bundle = getArguments();
-            int position = bundle.getInt("position");
+            final int position = bundle.getInt("position");
 
             Glide.with(this)
                     .load(mUriList.get(position))
                     .fitCenter()
                     .placeholder(R.drawable.ic_person)
                     .crossFade()
-                    .into(imageView);
+                    .into(new GlideDrawableImageViewTarget(imageView) {
+                        @Override
+                        public void onResourceReady(GlideDrawable drawable, GlideAnimation anim) {
+                            super.onResourceReady(drawable, anim);
+                            if( position == 0 ) {
+                                mMinScaleMatrix = new Matrix(imageView.getImageMatrix());
+                            }
+                        }
+                    });
 
             return swipeView;
         }
@@ -143,6 +151,86 @@ public class ImageViewerActivity extends AppCompatActivity {
             bundle.putInt("position", position);
             swipeFragment.setArguments(bundle);
             return swipeFragment;
+        }
+
+        @Override
+        public boolean onTouch(View v, MotionEvent event) {
+            ImageView view = (ImageView) v;
+            view.setScaleType(ImageView.ScaleType.MATRIX);
+
+            float scale;
+            switch (event.getAction() & MotionEvent.ACTION_MASK) {
+                case MotionEvent.ACTION_DOWN:   // first finger down only
+                    savedMatrix.set(matrix);
+                    start.set(event.getX(), event.getY());
+                    mode = DRAG;
+                    break;
+
+                case MotionEvent.ACTION_UP: // first finger lifted
+
+                case MotionEvent.ACTION_POINTER_UP: // second finger lifted
+
+                    mode = NONE;
+                    float resScale = getMatrixScale(matrix);
+                    if (resScale < 1.0) {
+                        matrix = new Matrix(mMinScaleMatrix);
+                    } else {
+                        break;
+                    }
+
+                    break;
+
+                case MotionEvent.ACTION_POINTER_DOWN: // first and second finger down
+
+                    oldDist = spacing(event);
+                    Log.d(TAG, "oldDist=" + oldDist);
+                    if (oldDist > 5f) {
+                        savedMatrix.set(matrix);
+                        midPoint(mid, event);
+                        mode = ZOOM;
+                        Log.d(TAG, "mode=ZOOM");
+                    }
+                    break;
+
+                case MotionEvent.ACTION_MOVE:
+
+                    if (mode == DRAG) {
+                        matrix.set(savedMatrix);
+                        matrix.postTranslate(event.getX() - start.x, event.getY() - start.y); // create the transformation in the matrix  of points
+                    } else if (mode == ZOOM) {
+                        // pinch zooming
+                        float newDist = spacing(event);
+                        Log.d(TAG, "newDist=" + newDist);
+                        if (newDist > 5f) {
+                            matrix.set(savedMatrix);
+                            scale = newDist / oldDist; // setting the scaling of the
+                            matrix.postScale(scale, scale, mid.x, mid.y);
+                        }
+                    }
+                    break;
+            }
+
+            view.setImageMatrix(matrix); // display the transformation on screen
+            return true; // indicate event was handled
+        }
+
+        private float spacing(MotionEvent event) {
+            float x = event.getX(0) - event.getX(1);
+            float y = event.getY(0) - event.getY(1);
+            return FloatMath.sqrt(x * x + y * y);
+        }
+
+        private void midPoint(PointF point, MotionEvent event) {
+            float x = event.getX(0) + event.getX(1);
+            float y = event.getY(0) + event.getY(1);
+            point.set(x / 2, y / 2);
+        }
+
+        float[] mTmpValues = new float[9];
+
+        private float getMatrixScale(Matrix matrix) {
+            matrix.getValues(mTmpValues);
+            return mTmpValues[Matrix.MSCALE_X];
         }
     }
 }
